0b99eace1e0c271a16bc23c95c48fa83195da43b
Nikolay <koliahndeko@gmail.com>
2020-07-03 00:00:34 +0300

    learning validate() method

---
+++ pages/users/_id.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <h1>Users {{this.$route.params.id}}</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    validate({params}) {
+        console.log('validate');
+        return /^\d+$/.test(params.id);
+    }
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -4,8 +4,18 @@
 
         <ul>
             <li v-for="user of 5" :key="user">
-                User {{user}}
+                <a href="#" @click.prevent="openUser(user)">User {{user}}</a>
             </li>
         </ul>
     </section>
 </template>
+
+<script>
+export default {
+    methods: {
+        openUser(user) {
+            this.$router.push(`/users/${user}`)
+        }
+    }
+}
+</script>
