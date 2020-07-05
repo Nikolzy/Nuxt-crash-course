ac578a77e60398ab36d3a3ff58637fcb8b7c8b16
Nikolay <koliahndeko@gmail.com>
2020-07-05 13:05:30 +0300

    nuxt-layouts

---
+++ pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <form action="">
+            <h1>Login page</h1>
+            <div class="form-group">
+                <input type="text" class="form-control">
+            </div>
+            
+            <p>
+                <nuxt-link to="/">Home page</nuxt-link>
+            </p>
+
+            <button class="btn btn-primary" type="submit">Login</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    layout: 'empty'
+}
+</script>
+
+<style scoped>
+    section {
+        max-width: 500px;
+        width: 100%;
+        margin: 0 auto;
+        padding: 0 20px;
+    }
+</style>
